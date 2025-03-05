eb28f032a2d2a9fa17c7c8be7e99ba9ac6bdb5f0
Diego Beltran <diegobeltran973@gmail.com>
2025-03-05 10:37:30 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -66,4 +66,6 @@ use App\Http\Controllers\ResourceController;
     Route::get('/simulaciones/phishing', [PracticasController::class, 'mostrarPhishing'])->name('mostrarPhishing');
     Route::get('/simulaciones/compras-seguras', [PracticasController::class, 'mostrarComprasSeguras'])->name('practicas.compras-seguras');
     Route::get('/simulaciones/wifi-publico', [PracticasController::class, 'mostrarWiFiPublico'])->name('practicas.wifi-publico');
+    
+    Route::post('/ask',[chatGPTController::class, 'askChat']);
 });
